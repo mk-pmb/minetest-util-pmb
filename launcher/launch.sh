cd2d41935008faa169b2f276eb8a5d04e1163e63
mk-pmb <mk-pmb@users.noreply.github.com>
2021-01-26 02:29:31 +0100

    Launcher: parse_cli

--- launcher/launch.sh
+++ launcher/launch.sh
@@ -15,18 +15,17 @@ function launch () {
   maybe_homesym .cache/minetest \
     .config/minetest/cache \
     || return $?
+
+  local -A CFG=()
+  local MT_EXTRA_ARGS=()
+  parse_cli "$@" || return $?
+
   # cd -- "$SELFPATH" || return $?
   cd -- "$HOME/.minetest" || return $?
-
-  local -A CFG=( [cfg_file]="$1" ); shift
-  [ -z "${CFG[cfg_file]}" ] \
-    || source -- "${CFG[cfg_file]}" || return $?$(
-    echo "E: failed to source config file '${CFG[cfg_file]}'" >&2)
-
   local MT_HOST="${CFG[host]}"
   local MT_PORT="${CFG[port]:-30000}"
   local MT_USER="${CFG[user]:-Guest}"
-  local MT_OPTS=(
+  local MT_OPT=(
     # --info
     # --verbose
     )
@@ -34,7 +33,7 @@ function launch () {
   if [ -n "$MT_HOST" ]; then
     MT_WORLD_NAME="$MT_HOST,$MT_PORT"
     mkdir --parents -- "worlds/$MT_WORLD_NAME"
-    MT_OPTS+=(
+    MT_OPT+=(
       --worldname "$MT_WORLD_NAME"
       --address "$MT_HOST"
       --port "$MT_PORT"
@@ -45,7 +44,7 @@ function launch () {
   local MT_PSWD="${CFG[pswd]}"
   if [ -n "$MT_PSWD" ]; then
     exec 17< <(echo -n "$MT_PSWD")
-    MT_OPTS+=( --password-file /dev/fd/17 --go )
+    MT_OPT+=( --password-file /dev/fd/17 --go )
   fi
 
   local LOG_FN=
@@ -54,15 +53,23 @@ function launch () {
   mkdir --parents -- "$(dirname -- "$LOG_FN")"
   >>"$LOG_FN" || return $?
   chmod a=,ug+rw -- "$LOG_FN" || return $?
+
+  local MT_PROG="$(which {~/.minetest/client/bin/,minetest} \
+    |& grep -Pe '^/' -m 1)"
+  [ -x "$MT_PROG" ] || return $?$(
+    echo "E: unable to find a minetest client executable." >&2)
+
+  local MT_CMD=(
+    stdbuf -i0 -o0 -e0
+    "$MT_PROG"
+    "${MT_OPT[@]}"
+    "${MT_EXTRA_ARGS[@]}"
+    )
+  echo -n "D: exec:"; printf ' ‹%s›' "${MT_CMD[@]}"; echo
+  ( sleep 2s; wmctrl -xFa Minetest.Minetest ) & disown $!
   exec &> >("$SELFPATH"/denoise_console_output/denoise.sh \
     | stdbuf -i0 -o0 -e0 tee -- "$LOG_FN")
-
-  ( sleep 2s; wmctrl -xFa Minetest.Minetest ) & disown $!
-  local PROG="$(which {~/.minetest/client/bin/,minetest} \
-    |& grep -Pe '^/' -m 1)"
-  [ -x "$PROG" ] || return $?$(
-    echo "E: unable to find a minetest client executable." >&2)
-  exec "$PROG" "${MT_OPTS[@]}" || return $?
+  exec "${MT_CMD[@]}" || return $?
 }
 
 
@@ -82,6 +89,25 @@ function maybe_homesym () {
   echo "E: not a directory: ~/$LINK, and neither are any of$DEST" >&2
   return 3
 }
+
+
+function parse_cli () {
+  local ARG=
+  while [ "$#" -ge 1 ]; do
+    ARG="$1"; shift
+    case "$ARG" in
+      -- ) break;;
+      -* ) echo "E: unsupported CLI argument: $ARG" >&2; return 3;;
+      *.rc )
+        source -- "$ARG" || return $?$(
+          echo "E: failed to source config file '$ARG'" >&2)
+        ;;
+      * ) echo "E: unsupported CLI argument: $ARG" >&2; return 3;;
+    esac
+  done
+  MT_EXTRA_ARGS+=( "$@" )
+}
+
 
 
 
