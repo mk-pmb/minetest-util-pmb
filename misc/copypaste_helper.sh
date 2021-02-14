8d8c2bc09bb94111e731970f9bb2cb02365ae4e0
mk-pmb <mk-pmb@users.noreply.github.com>
2021-02-14 05:13:36 +0100

    copy & paste_helper

---
+++ misc/copypaste_helper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function copypaste_helper () {
+  export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
+  cd /
+  local XC='xsel --clipboard'
+  local PREV= CRNT=
+  while sleep 0.1s; do
+    CRNT="$($XC --output)"
+    [ -n "$CRNT" ] || continue
+    [ "$CRNT" == "$PREV" ] && continue
+    if [ -n "$PREV" ]; then
+      printf '%(%T)T detected: "%s"\n' -1 "${CRNT//$'\n'/¶ }"
+      sleep 1s
+      printf '%(%T)T re-clipping.\n'
+      $XC --clear
+      $XC --input <<<"$CRNT"
+      [ -z "$*" ] || "$@" <<<"$CRNT" || return $?$(
+        echo "E: failed (rv=$?) to paste into $*" >&2)
+    fi
+    PREV="$CRNT"
+  done
+}
+
+
+
+
+
+
+
+
+
+
+copypaste_helper "$@"; exit $?
