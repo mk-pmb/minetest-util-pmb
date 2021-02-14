4e21faac234ed775c149a47fad72ac6960bac34e
mk-pmb <mk-pmb@users.noreply.github.com>
2021-02-14 20:42:12 +0100

    v0.0.4: copypaste_helper: cph_strip_colors, cph_merge_whitespace

--- misc/copypaste_helper.sh
+++ misc/copypaste_helper.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 
 
-function copypaste_helper () {
+function cph_lurk () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   [ "$#" -ge 1 ] || cd /
   local XC='xsel --clipboard'
@@ -12,7 +12,11 @@ function copypaste_helper () {
     [ -n "$CRNT" ] || continue
     [ "$CRNT" == "$PREV" ] && continue
     if [ -n "$PREV" ]; then
-      printf '%(%T)T detected: "%s"\n' -1 "${CRNT//$'\n'/¶ }"
+      PREV="$CRNT"  # now PREV means preview
+      PREV="${PREV//$'\n'/¶ }"
+      PREV="${PREV//$'\t'/␇$'\t'}"
+      PREV="${PREV//$'\x1B'/␛}"
+      printf '%(%T)T detected: "%s"\n' -1 "$PREV"
       sleep 1s
       printf '%(%T)T re-clipping.\n'
       $XC --clear
@@ -20,8 +24,20 @@ function copypaste_helper () {
       [ -z "$*" ] || "$@" <<<"$CRNT" || return $?$(
         echo "E: failed (rv=$?) to paste into $*" >&2)
     fi
-    PREV="$CRNT"
+    PREV="$CRNT"  # now PREV means previous
   done
+}
+
+
+function cph_strip_colors () {
+  < <(sed -urf <(echo '
+    s~\x1B\(c\@\#[0-9a-fA-F]{1,6}\)~~g  # strip color codes
+    ')) "$@"; return $?
+}
+
+
+function cph_merge_whitespace () {
+  < <(tr -s '\r\n \t' ' ') "$@"; return $?
 }
 
 
@@ -33,4 +49,4 @@ function copypaste_helper () {
 
 
 
-copypaste_helper "$@"; exit $?
+cph_lurk "$@"; exit $?
